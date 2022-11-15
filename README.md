d6cba426a5295f66b1a06fe2875ef994d2948a7b
Rand Asswad <rand.asswad@inria.fr>
2022-11-15 09:40:25 +0100

    Added decay and maintenance terms

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Implementations of chemostat models I am studying in my PhD.
 
 Jupyter notebooks:
 
-- [Growth of a single species](https://rand-asswad.github.io/chemostat-models/growth_single_minimal.html)
+- [Growth of a single species](https://rand-asswad.github.io/chemostat-models/growth_single.html)
--- model.py
+++ model.py
@@ -4,7 +4,10 @@ params = {
     'D': 0.8,
     'Sin': 2.0,
     'μmax': 1.0,
-    'ks': 0.2
+    'ks': 0.2,
+    'Y': 1.0,
+    'kd': 0,
+    'km': 0,
 }
 
 def mu(s, μmax=1.0, ks=0.2, **kw):
@@ -16,20 +19,23 @@ def mu_inv(r, μmax=1.0, ks=0.2, **kw):
 def mu_deriv(s, μmax=1.0, ks=0.2, **kw):
     return (μmax*ks) / (ks + s)**2
 
-def growth_rate(s, x, D=1.0, Sin=2.0, μmax=1.0, ks=0.2):
+def growth_rate(s, x, D=1.0, Sin=2.0, μmax=1.0, ks=0.2,
+                Y=1.0, km=0, kd=0):
     μ = mu(s, μmax=μmax, ks=ks)
-    ds = -μ*x + D*(Sin - s)
-    dx = (μ - D)*x
+    ds = -(μ/Y + km)*x + D*(Sin - s)
+    dx = (μ - D - kd)*x
     return ds, dx
 
-def isocline_ds(s, D=1.0, Sin=2.0, μmax=1.0, ks=0.2):
-    return D*(Sin - s) / mu(s, μmax=μmax, ks=ks)
+def isocline_ds(s, D=1.0, Sin=2.0, μmax=1.0, ks=0.2, Y=1.0, km=0, **kw):
+    return D*(Sin - s) / (mu(s, μmax=μmax, ks=ks)/Y + km)
 
-def equilibria(D=1.0, Sin=2.0, **kw):
+def equilibria(D=1.0, Sin=2.0, kd=0, **kw):
     E = [[Sin, 0]]
-    if D < mu(Sin, **kw) and D < kw['μmax']:
-        s1 = mu_inv(D, **kw)
-        E.append([s1, Sin - s1])
+    D_kd = D + kd
+    if D_kd < mu(Sin, **kw) and D_kd < kw['μmax']:
+        s1 = mu_inv(D_kd, **kw)
+        x1 = isocline_ds(s1, D=D, Sin=Sin, **kw)
+        E.append([s1, x1])
     return E
 
 # plots
@@ -38,10 +44,11 @@ def plot_isocline_dx(ax, **kwargs):
     color = kwargs.pop('color', 'b')
     label = kwargs.pop('label', '$\\dot{x}(t)=0$')
     D = kwargs.get('D', None)
+    kd = kwargs.get('kd', 0)
 
     ax.axhline(y=0, xmin=xmin, xmax=xmax, color=color, label=label)
-    if D:
-        ax.axvline(x=mu_inv(D, **kwargs), ymin=ymin, ymax=ymax, color=color)
+    if D and (D + kd) < kwargs['μmax']:
+        ax.axvline(x=mu_inv(D + kd, **kwargs), ymin=ymin, ymax=ymax, color=color)
     return ax
 
 
@@ -49,7 +56,6 @@ def plot_isocline_ds(ax, s=None, step=0.01, **kwargs):
     xmin, xmax, ymin, ymax = ax.axis()
     color = kwargs.pop('color', 'r')
     label = kwargs.pop('label', '$\\dot{s}(t)=0$')
-    D = kwargs.get('D', None)
 
     if not s:
         smin = xmin if xmin > 0 else step
